41f56d09980ed08e4a18d6387f4efbc0205932fc
ltdrobot <ltdrobot@localizethedocs.org>
2026-01-05 09:07:22 +0000

    Deploying to pages from @ localizethedocs/rust-patterns-l10n@44543980041df90c04d722f470b0835afefcf591 🚀

---
+++ ltd-config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",      "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/rust-patterns-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/rust-patterns-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/rust-patterns-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/rust-patterns-l10n"],
+  ]
+};
